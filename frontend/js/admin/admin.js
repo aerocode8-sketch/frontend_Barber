4d57cd02b25453ab9e38e9fdfdfb6e663c389ce3
Sebastia Galindo Mejia <sebastian.galindo01@uceva.edu.co>
2026-06-22 18:00:32 -0500

    Ampliacion hora de bloqueos

--- frontend/js/admin/admin.js
+++ frontend/js/admin/admin.js
@@ -81,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let appointments = [];
 
-    const timeSlots = ['08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM'];
+    const timeSlots = ['08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM'];
 
     const buttons = document.querySelectorAll('.nav-btn-item');
     const sections = document.querySelectorAll('.tab-content');
